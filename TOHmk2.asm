efe01abe55b529abdcdf2fe039deb553606c1961
Brandon Vickery <bvickery96@hotmail.com>
2016-06-28 21:56:14 -0700

    did a bit of organization and worked a bit on the logic of the recursion

--- TOHmk2.asm
+++ TOHmk2.asm
@@ -9,39 +9,7 @@
 
 .cseg
 .org 0x70
-	ldi r16, 0x21
-	ldi r17, 0xFF
-	sts SPH, r16
-	sts SPL, r17
-	ldi XH, high(disks)
-	ldi XL, low(disks)
-	ldi r16, 0x05 ;low byte
-	ldi r17, 0x00 ; high byte
-	st X+, r16
-	st X, r17
-	ldi XH, high(disks)
-	ldi XL, low(disks)
-	push XH
-	push XL
-	ldi r16, 0x01 ;starting
-	ldi r17, 0x02 ;spare
-	ldi r18, 0x03 ;final		
-	ldi XH, high(currentL)
-	ldi XL, low(currentL)
-	st X, r16
-	push XH
-	push XL
-	ldi XH, high(spareL)
-	ldi XL, low(spareL)
-	st X, r17
-	push XH
-	push XL
-	ldi XH, high(finalL)
-	ldi Xl, low(finalL)
-	st X, r18
-	push XH
-	push XL
-	
+	call init_params
 	clr r0
 	call TOH
 	pop r16
@@ -75,38 +43,43 @@ TOH:
 	push r19 ;spareL
 	push r20 ;finalL
 	push r21 ;temp reg
+	push r22
 	lds ZH, SPH
 	lds ZL, SPL
-	ldd XH, Z+23
-	ldd XL, Z+22
+	ldd XH, Z+24
+	ldd XL, Z+23
 	ld r16, X+
 	ld r17, X
 	cpi r16, 0x00
-	brne low_byte_not_zero
+	brne low_byte_not_zero1
 	cpi r17, 0x00
-	brne high_byte_not_zero
+	brne high_byte_not_zero1
 	jmp finish
 
-high_byte_not_zero:
+high_byte_not_zero1:
 	call check_r17 ;we have now stored the new number of disks in memory and pushed their address
-	jmp skip_check ;on the stack
-low_byte_not_zero:
-	ldd XH, Z+23
-	ldd XL, Z+22
+	jmp do_check1 ;on the stack
+low_byte_not_zero1:
+	ldd XH, Z+24
+	ldd XL, Z+23
 	dec r16
 	st X, r16
-	brne skip_check
-	call check_r17
-skip_check:
+	brne skip_check1
+	call check_r17 
+;need to change check_17 so that it does not push items onto the stack because it dosent happen
+;everytime and that will fuck up the recusion. Actually it may not...
+do_check1:
+	
+skip_check1:
 		
-	ldd YH, Z+23
-	ldd YL, Z+22
+	ldd YH, Z+24
+	ldd YL, Z+23
 	push YH	;pushing current address on the stack
 	push YL	
-	ldd XH, Z+23
-	ldd XL, Z+22
-	ldd YH, Z+21
-	ldd YL, Z+20
+	ldd XH, Z+24
+	ldd XL, Z+23
+	ldd YH, Z+22
+	ldd YL, Z+21
 	ld r19, X
 	ld r20, Y
 	mov r21, r19
@@ -132,14 +105,48 @@ check_r17:
 	ldi r16, 0xFF
 	push XH
 	push XL
-	ldd XH, Z+25
-	ldd XL, Z+24
+	ldd XH, Z+26
+	ldd XL, Z+25
 	st X+, r16
 	st X, r17
-
 skip:
 	ret
 
+;intializes the parameters
+init_params:
+	ldi r16, 0x21
+	ldi r17, 0xFF
+	sts SPH, r16
+	sts SPL, r17
+	ldi XH, high(disks)
+	ldi XL, low(disks)
+	ldi r16, 0x05 ;low byte
+	ldi r17, 0x00 ; high byte
+	st X+, r16
+	st X, r17
+	ldi XH, high(disks)
+	ldi XL, low(disks)
+	push XH
+	push XL
+	ldi r16, 0x01 ;starting
+	ldi r17, 0x02 ;spare
+	ldi r18, 0x03 ;final		
+	ldi XH, high(currentL)
+	ldi XL, low(currentL)
+	st X, r16
+	push XH
+	push XL
+	ldi XH, high(spareL)
+	ldi XL, low(spareL)
+	st X, r17
+	push XH
+	push XL
+	ldi XH, high(finalL)
+	ldi Xl, low(finalL)
+	st X, r18
+	push XH
+	push XL
+	ret
 .dseg
 .org 0x200
 disks: .byte 2
